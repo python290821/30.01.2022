6778608857725d658a41c3b9f2ea8239f9b0688e
micserv010821 <dotnet290320@gmail.com>
2022-01-30 20:56:31 +0200

    upload

--- main.py
+++ main.py
@@ -1,17 +1,67 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 from kivy.uix.widget import Widget
-from kivy.lang.builder import Builder
 from kivy.properties import ObjectProperty, StringProperty
+# drop-down menu is a list of items that
+# appear whenever a piece of text or a
+# button is clicked.
+# To use drop down you must have ti import it
+from kivy.uix.dropdown import DropDown
 
-class DbGenWidget(Widget):
+# module consists the floatlayout
+# to work with FloatLayout first
+# you have to import it
+from kivy.uix.floatlayout import FloatLayout
 
-    slabel1 = StringProperty('On')
-    slabel2 = StringProperty('Off')
+# The Button is a Label with associated actions that
+# are triggered when the button is pressed (
+# or released after a click / touch).
+from kivy.uix.button import Button
+
+class CustomDropDown(DropDown):
+    pass
+
+class MyGrid(Widget):
+
+    airline_companies = ObjectProperty(None)
+    customers = ObjectProperty(None)
+    administrators = ObjectProperty(None)
+    flights_per_company = ObjectProperty(None)
+    tickets_per_customer = ObjectProperty(None)
+    countries = ObjectProperty(None)
+
+    # State labels
+    slabel1 = StringProperty()
+    slabel2 = StringProperty()
+
+    def btn(self):
+        print("Airline Companies:", self.airline_companies.text,
+              "Customers:", self.customers.text,
+              "Administrators:", self.administrators.text,
+              "Flights Per Company:", self.flights_per_company.text,
+              "Tickets Per Customer:", self.tickets_per_customer.text,
+              "Countries:", self.countries.text)
+
+        self.airline_companies.text = ""
+        self.customers.text = ""
+        self.administrators.text = ""
+        self.flights_per_company.text = ""
+        self.tickets_per_customer.text = ""
+        self.countries.text = ""
+
+    # Callback for the checkbox
+    def checkbox_click(self, instance, value):
+        if value is True:
+            print("Checkbox Checked")
+        else:
+            print("Checkbox Unchecked")
+
+    def savedstate(self):
+        # Here we can retrieve user saved radio button state if one exists
+        # Assign optional label values
+        self.slabel1 = 'On'
+        self.slabel2 = 'Off'
+        return ['down', 'normal']
 
     def switchstate1(self):
         # Switch radio button 1 on and process event trigger
@@ -20,7 +70,8 @@ class DbGenWidget(Widget):
         # Update optional label values
         self.slabel1 = 'on'
         self.slabel2 = 'off'
-        print(self.ids.rbutton1.state, self.ids.rbutton2.state)
+        print
+        self.ids.rbutton1.state, self.ids.rbutton2.state
 
     def switchstate2(self):
         # Switch radio button 2 on and process event trigger
@@ -29,24 +80,19 @@ class DbGenWidget(Widget):
         # Update optional label values
         self.slabel1 = 'off'
         self.slabel2 = 'on'
-        print(self.ids.rbutton1.state, self.ids.rbutton2.state)
+        print (self.ids.rbutton1.state, self.ids.rbutton2.state)
 
+
+    # Get dropdown value when clicked
     def spinner_clicked(self, value):
         # get selected dropdown value and change the label text
         self.ids.selected_value.text = f"You have selected {value}"
 
-def btn(self):
-    pass
 
-class MyApp(App):   
+class MyApp(App):
     def build(self):
-        return DbGenWidget()
-    # app.btn() in kv file
-    def btn(self):
-        print('clicked...')        
+        return MyGrid()
 
-
-Builder.load_file('my1.kv')
 
 if __name__ == "__main__":
     MyApp().run()
