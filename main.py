916cad2be24a68cace592e8b716eb953c99aa07a
micserv010821 <dotnet290320@gmail.com>
2022-01-30 20:02:12 +0200

    upload

--- main.py
+++ main.py
@@ -6,15 +6,28 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.lang.builder import Builder
+from kivy.properties import ObjectProperty
 
-class MyWidget(Widget):
+class DbGenWidget(Widget):
+    airline_companies = ObjectProperty(None)
+    customers = ObjectProperty(None)
+    administrators = ObjectProperty(None)
+    flights_per_company = ObjectProperty(None)
+    tickets_per_customer = ObjectProperty(None)
+    countries = ObjectProperty(None)
+
     # root.btn() in kv file
     def btn(self):
-        print('clicked...')
+        print("Airline Companies:", self.airline_companies.text,
+            "Customers:", self.customers.text,
+            "Administrators:", self.administrators.text,
+            "Flights Per Company:", self.flights_per_company.text,
+            "Tickets Per Customer:", self.tickets_per_customer.text,
+            "Countries:", self.countries.text)
    
-class MyApp(App):
+class MyApp(App):   
     def build(self):
-        return MyWidget()
+        return DbGenWidget()
     # app.btn() in kv file
     def btn(self):
         print('clicked...')        
